aa03f9bcf00f8734be5bddc3fca826893e8f325a
Jainendra <jainendra.mahajan456@gmail.com>
2023-09-15 18:28:45 +0530

    Added JSX Component & Element

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = [React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"}, 
-[React.createElement("h1", {}, "This is h1"), React.createElement("h2", {}, "This is h2")])
-),
+// const heading = React.createElement("h1" , {id: "heading"}, "Hello World from React");
 
-React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"}, 
-[React.createElement("h1", {}, "This is h1"), React.createElement("h2", {}, "This is h2")])
-)];
+// jsx --> React.createElement --> JS object --> HTML Element
+const jsxHeading = <h1 id= "heading">  Hello World from JSX</h1>
+
+const Title = () => (<h1>This is the title from another component</h1>)
+
+const title = (<h1>This is title element</h1>);
+
+//React component
+const HeadingComponent = () => (
+    <div>
+        {title}
+        <Title/>
+        {Title()}
+        <Title></Title>
+        {/* All the three title above are of same use */}
+        <h1>This is Heading Component</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //This root is for DOM to connect it with JS.
 
-root.render(heading);
+root.render(<HeadingComponent/>);
 //Taking heading and converting into HTML tag and render on root.
