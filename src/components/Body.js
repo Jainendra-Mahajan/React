f98edd16f25f8539f7e1a0efcce0cb0dfe66cc75
Jainendra <jainendra.mahajan456@gmail.com>
2023-10-02 18:49:02 +0530

    Added functional search Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,21 +2,43 @@ import ResCard from "./ResCard";
 import { useState } from "react";
 import resList from "../utils/mockData";
 
+
+function filterData(searchText , restaurantList) {
+    const data = restaurantList.filter((res) => 
+        res.info.name.toLowerCase().includes(searchText.toLowerCase())
+    )
+    return data;
+}
 const Body = () => {
+    const [searchText , setsearchText] = useState("");
     const [restaurantList , setRestaurantList] = useState(resList);
     
     return (
         <div className="body">
             <div className="filter">
+
+                <input 
+                placeholder="Enter Restaurant Name"
+                value={searchText}
+                onChange={(e) => setsearchText(e.target.value)}/>
+
+                <button className="filter-btn" 
+                    onClick={() => { const data = filterData (searchText, restaurantList);
+                        if(data.length > 0) setRestaurantList(data);
+                        else setRestaurantList(resList);
+                }}>
+                  search  
+                </button>
+
                 <button 
                     className="filter-btn" 
                     onClick={() => {
                         const filterData = restaurantList.filter(
                             (res) => res.info.avgRating > 4
                         );
-                        setRestaurantList(filterData);
+                        setRestaurantList(filterData)
                     }}
-                    > Top Rated Restautants
+                    > Top Rated Restaurants
                 </button>
             </div>
             <div className="card">
